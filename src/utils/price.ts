a02bc375b6d2d265ec23ec536c30300c66445603
exhyn <exhyn@protonmail.com>
2021-05-13 00:41:30 -0300

    fix copypaste

--- src/utils/price.ts
+++ src/utils/price.ts
@@ -1,4 +1,4 @@
-const ENDPOINT_PRICE_USD = 'https://mirror.yogi.finance/prices';
+const ENDPOINT_PRICE_USD = 'https://mirror.yogi.fi/prices';
 
 export async function getPrices(assets: string[]): Promise<Record<string, number>> {
     const url = `${ENDPOINT_PRICE_USD}`;
