f8cfe5371c0a3c87cad082ce611ff9abc024a065
exhyn <exhyn@protonmail.com>
2021-05-18 06:30:03 -0300

    match landing hue

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,7 @@ export default defineComponent({
 }
 
 [data-theme="dark"] {
-    --background-primary: radial-gradient(circle at 0% 0%, #4B0082, #1a2361);
+    --background-primary: radial-gradient(circle at 0% 0%, #330082, #1a2361);
     --background-secondary: #21222c;
     --background-control: #1f2029;
     --background-hover: #20222c;
