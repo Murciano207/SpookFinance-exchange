e6bfdffb04aa584c21e60c47f7b04f0d5c34600c
exhyn <exhyn@protonmail.com>
2021-05-26 18:43:09 -0300

    improve stability through redundance

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -2,7 +2,7 @@ import { JsonRpcProvider } from '@ethersproject/providers';
 
 import config from '@/config';
 
-// TODO: randomize rpc
-const provider = new JsonRpcProvider(config.rpc[0], config.chainId);
+const idx = Math.floor(Math.random() * config.rpc.length);
+const provider = new JsonRpcProvider(config.rpc[idx]);
 
 export default provider;
